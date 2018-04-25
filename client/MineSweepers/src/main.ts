9fe01c0f9f4b9864bdc12ca81b115552dab685c8
Alex Wong <awsk1994@gmail.com>
2018-04-25 01:43:19 -0400

    Remove unnecessary code.

--- client/MineSweepers/src/main.ts
+++ client/MineSweepers/src/main.ts
@@ -3,9 +3,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { SocketService } from './app/socket.service';
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule, [SocketService]);
+platformBrowserDynamic().bootstrapModule(AppModule);
